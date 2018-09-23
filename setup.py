bc63d999bd066403dbf0e0c61b3da2143d92d169
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2018-09-23 14:00:49 +0200

    Version 1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name = 'PyQNormaliz',
-    version = '1.1',
+    version = '1.2',
     description = 'An interface to Normaliz',
     author = 'Sebastian Gutsche, Richard Sieg',
     author_email = 'sebastian.gutsche@gmail.com',
